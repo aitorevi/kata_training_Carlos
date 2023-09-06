adc9719a7564c14dcfc9722e00efd5b5e80d4bb6
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-09-06 11:46:16 +0200

    test(red): counts factorizations for numbers with more than one digit

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -6,11 +6,16 @@ describe("Factorize", () => {
     it("can´t factorize single digit numbers", () => {
         expect(countFactorizations(5)).toBe(0)
     })
+
+    it("counts factorizations for numbers with more than one digit", () => {
+        expect(countFactorizations(33)).toBe(1)
+    })
 })
 
 // Codewars
 
 // 123 -> 1 * 2 * 3 = 6 - descompuesto en 1 iteracion
+// 33 -> 3 * 3 = 9
 // 99 -> 9 * 9 = 81 , 8 * 1 = 8 - descompuesto en 2 iteraciones
 // 5 -> 0 iteraciones
 // 
