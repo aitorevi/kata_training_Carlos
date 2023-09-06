92b2e7b4d991a0b2273acec18360c519a043f0d0
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-09-06 11:36:11 +0200

    test(red): can´t factorize single digit numbers

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,7 +1,13 @@
-import { saidHello} from "../core/example";
-
-describe("Hello", () => {
-    it("world!", () => {
-        expect(saidHello("world")).toBe("Hello world!")
+describe("Factorize", () => {
+    it("can´t factorize single digit numbers", () => {
+        expect(countFactorizations(5)).toBe(0)
     })
 })
+
+// Codewars
+
+// 123 -> 1 * 2 * 3 = 6 - descompuesto en 1 iteracion
+// 99 -> 9 * 9 = 81 , 8 * 1 = 8 - descompuesto en 2 iteraciones
+// 5 -> 0 iteraciones
+// 
+
